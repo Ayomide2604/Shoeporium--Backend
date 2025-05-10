404dba855e244404ace40f110cb883615ac5d2d0
Ayomide2604 <106402640+Ayomide2604@users.noreply.github.com>
2025-05-10 15:41:42 +0100

    Fix CORS_ALLOWED_ORIGINS in settings.py by removing trailing slash from production frontend URL for consistency.

--- shoeporium/settings.py
+++ shoeporium/settings.py
@@ -179,7 +179,7 @@ CORS_ALLOW_METHODS = (
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
-    "https://shoeporium-frontend.vercel.app/",
+    "https://shoeporium-frontend.vercel.app",
 ]
 
 # Cloudinary settings
